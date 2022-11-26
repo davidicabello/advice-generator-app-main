adefe270e492c720e7e295e6482de371e9c935b0
davidlp90 <davidivan.eda@gmail.com>
2022-11-25 21:25:47 -0300

    updates and js

---
+++ public/app.js
@@ -0,0 +1,27 @@
+const resDiv = document.querySelector('.results');
+const resBtn = document.querySelector('#data');
+
+
+
+resBtn.addEventListener('click', () => {
+    getAdvice();
+});
+
+window.onload = () => {
+    getAdvice();
+};
+
+
+
+function getAdvice() {
+
+    fetch('	https://api.adviceslip.com/advice').then(response => {
+        return response.json();
+    }).then(adviceData => {
+        const Adviceobj = adviceData.slip;
+        resDiv.innerHTML = `<h1><i class="fa-solid fa-quote-left"></i>${Adviceobj.advice} <i class="fa-solid fa-quote-right"></i></h1>`;
+    }).catch(error => {
+        console.log(error);
+    });
+
+}
