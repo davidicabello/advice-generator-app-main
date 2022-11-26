fd34e289939a29a8826bc333e8c97497e4f462b9
davidlp90 <davidivan.eda@gmail.com>
2022-11-26 01:06:55 -0300

    final update

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const resDiv = document.querySelector('.results');
 const resBtn = document.querySelector('#data');
-
+const advNumbDiv = document.querySelector('.advice-number');
 
 
 resBtn.addEventListener('click', () => {
@@ -20,6 +20,7 @@ function getAdvice() {
     }).then(adviceData => {
         const Adviceobj = adviceData.slip;
         resDiv.innerHTML = `<h1><i class="fa-solid fa-quote-left"></i>${Adviceobj.advice} <i class="fa-solid fa-quote-right"></i></h1>`;
+        advNumbDiv.innerHTML = `<p>ADVICE # ${Adviceobj.id}</p>`
     }).catch(error => {
         console.log(error);
     });
